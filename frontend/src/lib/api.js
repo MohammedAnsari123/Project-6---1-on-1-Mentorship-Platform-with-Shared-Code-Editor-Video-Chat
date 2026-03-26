1e71e4ec681a4be3c3dbbcb444b63f109344b3ed
MohammedAnsari123 <ansarimohammed213161@gmail.com>
2026-03-26 20:17:03 +0530

    sd

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://project-6-1-on-1-mentorship-platform.onrender.com/api',
 });
 
 // Add token to headers if it exists
